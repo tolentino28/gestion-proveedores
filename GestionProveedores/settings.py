155192d447902a23db488150cb3fbcb187b31718
yuliana <yulianatolentinosandoval@gmail.com>
2024-12-13 13:02:31 -0500

    Instalaciones del servidor

--- GestionProveedores/settings.py
+++ GestionProveedores/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'GestionProveedores.urls'
@@ -69,11 +72,21 @@ WSGI_APPLICATION = 'GestionProveedores.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+# Replace the SQLite DATABASES configuration with PostgreSQL:
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/gestion-proveedores',
+        conn_max_age=600
+    )
 }
 
 
@@ -112,6 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Front/static')]
 
 # Default primary key field type
@@ -119,7 +141,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Front/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+#import os
 
 # Configuración para archivos multimedia
 MEDIA_URL = '/media/'
